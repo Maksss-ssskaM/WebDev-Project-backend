cf4862efa1f4ceb8cc67e2964593788e6f661408
Maks <maks.shein.2002@mail.ru>
2023-04-09 04:27:41 +0300

    Removed unnecessary methods in controllers

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,30 +1,24 @@
-import {Body, Controller, Post, UseGuards} from '@nestjs/common';
-import {AuthService} from "./auth.service";
-import {CreateUserDTO} from "../users/dto";
-import {UserLoginDTO} from "./dto";
-import {AuthUserResponse} from "./responce";
-import {ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDTO } from '../users/dto';
+import { UserLoginDTO } from './dto';
+import { AuthUserResponse } from './responce';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/guards/jwt-guard';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
-    @ApiTags('API')
-    @ApiResponse({status: 201, type: AuthUserResponse})
-    @Post('register')
-    register(@Body() dto: CreateUserDTO): Promise<AuthUserResponse>{
-        return this.authService.registerUsers(dto)
-    }
-    @ApiTags('API')
-    @ApiResponse({status: 200, type: AuthUserResponse})
-    @Post('login')
-    login(@Body() dto: UserLoginDTO): Promise<AuthUserResponse>{
-        return this.authService.loginUser(dto)
-    }
-
-    @UseGuards(JwtAuthGuard)
-    @Post('test')
-    test() {
-        return true
-    }
+  constructor(private readonly authService: AuthService) {}
+  @ApiTags('API')
+  @ApiResponse({ status: 201, type: AuthUserResponse })
+  @Post('register')
+  register(@Body() dto: CreateUserDTO): Promise<AuthUserResponse> {
+    return this.authService.registerUsers(dto);
+  }
+  @ApiTags('API')
+  @ApiResponse({ status: 200, type: AuthUserResponse })
+  @Post('login')
+  login(@Body() dto: UserLoginDTO): Promise<AuthUserResponse> {
+    return this.authService.loginUser(dto);
+  }
 }
